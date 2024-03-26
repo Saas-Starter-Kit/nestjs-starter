19c20e3d06350c6311d1202e5dc291f9a915f766
iqbal125 <iqbal125sf@gmail.com>
2024-03-26 12:16:49 -0700

    remove users controller

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,10 +1,9 @@
 // src/users/users.module.ts
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
 
 @Module({
-  controllers: [UsersController],
+  controllers: [],
   providers: [UsersService],
   exports: [UsersService],
 })
